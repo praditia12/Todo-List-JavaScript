597211c7a98dd8ffb6d9c3245a5ccc9810675da1
praditia12 <aditpraditia714@gmail.com>
2023-12-21 22:18:05 +0700

    added code javascript

---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const submitForm = document.getElementById("form")
+    submitForm.addEventListener("submit", function (event) {
+        event.preventDefault()
+        addTodo()
+    })
+})
+
+function addTodo() {
+    const textTodo = document.getElementById("title").value
+    const timestamp = document.getElementById("date").value
+
+    const generatedID = generateId()
+    const todoObject = generateTodoObject(generatedID, textTodo, timestamp, false)
+    todos.push(todoObject)
+
+    document.dispatchEvent(new Event(RENDER_EVENT))
+}
+
+function generateId() {
+    return +new Date()
+}
+
+function generateTodoObject(id, task, timestamp, isCompleted) {
+    return {
+        id,
+        task,
+        timestamp,
+        isCompleted,
+    }
+}
+
+const todos = []
+const RENDER_EVENT = "render-todo"
+
+document.addEventListener(RENDER_EVENT, function () {
+    console.log(todos)
+})
