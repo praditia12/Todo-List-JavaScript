18b20b5cd0d44f0bb6e92f3459acb722555c81c2
aditpraditia <adit.praditia@quatronema.co.id>
2023-12-22 10:34:10 +0700

    menampilkan todo selesai dan menghapus todo

--- js/script.js
+++ js/script.js
@@ -38,11 +38,13 @@ document.addEventListener(RENDER_EVENT, function () {
     const uncompletedTODOList = document.getElementById("todos");
     uncompletedTODOList.innerHTML = "";
 
+    const completedTODOList = document.getElementById("completed-todos");
+    completedTODOList.innerHTML = "";
+
     for (const todoItem of todos) {
         const todoElement = makeTodo(todoItem);
-        if (!todoItem.isCompleted) {
-            uncompletedTODOList.append(todoElement);
-        }
+        if (!todoItem.isCompleted) uncompletedTODOList.append(todoElement);
+        else completedTODOList.append(todoElement);
     }
 });
 
@@ -108,4 +110,32 @@ function findTodo(todoId) {
         }
     }
     return null;
+}
+
+function removeTaskFromCompleted(todoId) {
+    const todoTarget = findTodoIndex(todoId);
+
+    if (todoTarget === -1) return;
+
+    todos.splice(todoTarget, 1);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function undoTaskFromCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+
+    if (todoTarget == null) return;
+
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodoIndex(todoId) {
+    for (const index in todos) {
+        if (todos[index].id === todoId) {
+            return index;
+        }
+    }
+
+    return -1;
 }
